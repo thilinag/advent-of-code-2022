92300f2451cb7a844cf3c9aafde4972ee5176f30
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-01 16:02:54 +1030

    add day1 comments

--- day1/part1.mjs
+++ day1/part1.mjs
@@ -5,8 +5,9 @@ const data = input.split('\n\n')
 
 console.log(
   data
-    .map(elf => elf.split('\n')
-    .reduce((p, c) => p + Number(c), 0))
-    .sort((a, b) => a - b)
-    .at(-1)
+    .map(elf => elf.split('\n') // get each elf's calorie total as an array
+      .reduce((p, c) => p + Number(c), 0)
+    ) 
+    .sort((a, b) => b - a) // sort it DESC
+    .at(0) // highest calorie elf
 )
--- day1/part2.mjs
+++ day1/part2.mjs
@@ -5,9 +5,10 @@ const data = input.split('\n\n')
 
 console.log(
     data
-        .map(elf => elf.split('\n')
-        .reduce((p, c) => p + Number(c), 0))
-        .sort((a, b) => a - b)
-        .slice(-3)
-        .reduce((p, c) => p + c, 0)
+        .map(elf => elf.split('\n') // get each elf's calorie total as an array
+            .reduce((p, c) => p + Number(c), 0)
+        ) 
+        .sort((a, b) => b - a) // sort it DESC
+        .slice(0, 3) // get top 3 highest calorie elves
+        .reduce((p, c) => p + c, 0) // get calorie total of top 3 highest calorie elves
 )
