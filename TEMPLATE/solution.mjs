ad7c38a86a08b3a255282d63d5075b30ef7598cc
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-02 16:53:23 +1030

    add template

---
+++ TEMPLATE/solution.mjs
@@ -0,0 +1,18 @@
+import { readFileSync } from 'fs'
+const input = readFileSync('./input.txt', { encoding: 'utf8' })
+const data = input.split('\n')
+
+const part1 = (data) => {
+
+
+  console.log()
+}
+
+const part2 = (data) => {
+
+
+  console.log()
+}
+
+part1(data)
+part2(data)
