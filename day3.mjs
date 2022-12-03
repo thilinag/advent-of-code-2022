5b8d962a1c2aa0cb71846f4e62122b0280662fff
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-03 16:46:58 +1030

    day 3 solve part 2

--- day3.mjs
+++ day3.mjs
@@ -2,6 +2,12 @@ import { readFileSync } from "fs";
 const input = readFileSync("./input.txt", { encoding: "utf8" });
 const data = input.split("\n");
 
+const getItemPriority = (item) => {
+  // item types a through z have priorities 1 through 26.
+  // item types A through Z have priorities 27 through 52.
+  return item.charCodeAt() - (/[a-z]/.test(item) ? 96 : 38);
+};
+
 const part1 = (rucksacks) => {
   const rucksackCommonItems = rucksacks.map((rucksack) => {
     const firstCompartment = rucksack.substr(0, rucksack.length / 2);
@@ -11,15 +17,32 @@ const part1 = (rucksacks) => {
       .find((item) => secondCompartment.indexOf(item) > -1);
   });
   const itemPriorities = rucksackCommonItems.map((item) => {
-    return item.charCodeAt() - (/[a-z]/.test(item) ? 96 : 38);
+    return getItemPriority(item);
   });
   console.log(
     itemPriorities.reduce((sum, itemPriority) => sum + itemPriority, 0)
   );
 };
 
-const part2 = (data) => {
-  console.log();
+const part2 = (rucksacks) => {
+  const rucksackBadges = [];
+  for (let i = 0; i < rucksacks.length; i = i + 3) {
+    const commonItem = rucksacks[i]
+      .split("")
+      .find(
+        (item) =>
+          rucksacks[i + 1].indexOf(item) > -1 &&
+          rucksacks[i + 2].indexOf(item) > -1
+      );
+    rucksackBadges.push(commonItem);
+  }
+
+  const itemPriorities = rucksackBadges.map((item) => {
+    return getItemPriority(item);
+  });
+  console.log(
+    itemPriorities.reduce((sum, itemPriority) => sum + itemPriority, 0)
+  );
 };
 
 part1(data);
