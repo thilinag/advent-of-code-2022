3c02dee226edadfa5d6311c464de426a7ae0c7a2
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-03 11:27:26 +1030

    refactor day2 to remove unnecessary maps

--- day2/solution.mjs
+++ day2/solution.mjs
@@ -2,57 +2,49 @@ import { readFileSync } from 'fs'
 const input = readFileSync('./input.txt', { encoding: 'utf8' })
 const data = input.split('\n')
 
-const OutcomeScore = {
-  'A X': 3,
-  'B Y': 3,
-  'C Z': 3,
-  'A Y': 6,
-  'A Z': 0,
-  'B X': 0,
-  'B Z': 6,
-  'C X': 6,
-  'C Y': 0,
-}
-
-const endScore = {
-  'X': 0,
-  'Y': 3,
-  'Z': 6
-}
-
 const opponentChoices = ['A', 'B', 'C']
-const myChoices = ['X', 'Y', 'Z']
+const ourChoices = ['X', 'Y', 'Z']
 
 const part1 = (data) => {
   const turns = data.map(
-    turn => OutcomeScore[turn] + myChoices.indexOf(turn.at(-1)) + 1
+    turn => {
+      const [opponentChoice, ourChoice] = turn.split(' ')
+      const opponentMove = opponentChoices.indexOf(opponentChoice) + 1
+      const ourMove = ourChoices.indexOf(ourChoice) + 1
+      
+      if (opponentMove === ourMove) { // draw
+        return 3 + ourMove
+      }
+
+      if (ourMove - opponentMove === 1 || ourMove - opponentMove === -2) { // won
+        return 6 + ourMove
+      }
+
+      return ourMove // lost
+    }
   )
-  
+
   console.log(turns.reduce((prevSum, turnScore) => prevSum + turnScore, 0 ))
 }
 
 const part2 = (data) => {
-  const turns = data.map(turn => {
-    let myChoice;
-    const [opponentChoice, howRoundEnds] = turn.split(' ');
+  const turns = data.map(
+    turn => {
+      const [opponentChoice, outcome] = turn.split(' ')
+      const opponentMove = opponentChoices.indexOf(opponentChoice) + 1
+      
+      if (outcome === 'X') { // need to loose
+        return opponentMove - 1 || 3
+      }
 
-    switch(howRoundEnds) {
-      case 'X':
-        myChoice = myChoices[opponentChoices.indexOf(opponentChoice) - 1] || myChoices[2]
-        break
+      if (outcome === 'Y') { // need to loose
+        return 3 + opponentMove
+      }
 
-      case 'Y':
-        myChoice = myChoices[opponentChoices.indexOf(opponentChoice)]
-        break
-          
-      case 'Z':
-        myChoice = myChoices[opponentChoices.indexOf(opponentChoice) + 1] ||  myChoices[0]
-        break
+      return ((opponentMove + 1) > 3 ? 1 : (opponentMove + 1)) + 6 // need to win
     }
+  )
 
-    return endScore[howRoundEnds] + myChoices.indexOf(myChoice) + 1
-  })
-  
   console.log(turns.reduce((prevSum, turnScore) => prevSum + turnScore, 0 ))
 }
 
