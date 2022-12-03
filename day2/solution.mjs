1ad92a94dcd28365e969e6ead38a2199e52a4506
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-03 11:53:45 +1030

    add prettier formatting

--- day2/solution.mjs
+++ day2/solution.mjs
@@ -1,52 +1,58 @@
-import { readFileSync } from 'fs'
-const input = readFileSync('./input.txt', { encoding: 'utf8' })
-const data = input.split('\n')
+import { readFileSync } from "fs";
+const input = readFileSync("./input.txt", { encoding: "utf8" });
+const data = input.split("\n");
 
-const opponentChoices = ['A', 'B', 'C']
-const ourChoices = ['X', 'Y', 'Z']
+const opponentChoices = ["A", "B", "C"];
 
 const part1 = (data) => {
-  const turns = data.map(
-    turn => {
-      const [opponentChoice, ourChoice] = turn.split(' ')
-      const opponentMove = opponentChoices.indexOf(opponentChoice) + 1
-      const ourMove = ourChoices.indexOf(ourChoice) + 1
-      
-      if (opponentMove === ourMove) { // draw
-        return 3 + ourMove
-      }
+  const ourChoices = ["X", "Y", "Z"];
+  const turns = data.map((turn) => {
+    const [opponentChoice, ourChoice] = turn.split(" ");
+    const opponentMove = opponentChoices.indexOf(opponentChoice) + 1;
+    const ourMove = ourChoices.indexOf(ourChoice) + 1;
 
-      if (ourMove - opponentMove === 1 || ourMove - opponentMove === -2) { // won
-        return 6 + ourMove
-      }
-
-      return ourMove // lost
+    if (opponentMove === ourMove) {
+      // draw
+      return 3 + ourMove;
     }
-  )
 
-  console.log(turns.reduce((prevSum, turnScore) => prevSum + turnScore, 0 ))
-}
+    if (ourMove - opponentMove === 1 || ourMove - opponentMove === -2) {
+      // won
+      return 6 + ourMove;
+    }
+
+    return ourMove; // lost
+  });
+
+  console.log(turns.reduce((prevSum, turnScore) => prevSum + turnScore, 0));
+};
 
 const part2 = (data) => {
-  const turns = data.map(
-    turn => {
-      const [opponentChoice, outcome] = turn.split(' ')
-      const opponentMove = opponentChoices.indexOf(opponentChoice) + 1
-      
-      if (outcome === 'X') { // need to loose
-        return opponentMove - 1 || 3
-      }
+  const turns = data.map((turn) => {
+    const [opponentChoice, outcome] = turn.split(" ");
+    const opponentMove = opponentChoices.indexOf(opponentChoice) + 1;
 
-      if (outcome === 'Y') { // need to loose
-        return 3 + opponentMove
-      }
-
-      return ((opponentMove + 1) > 3 ? 1 : (opponentMove + 1)) + 6 // need to win
+    if (outcome === "X") {
+      // need to loose
+      return opponentMove - 1 || 3; // play the move which comes before the order of opponent move to lose, check for out of bounds
     }
-  )
 
-  console.log(turns.reduce((prevSum, turnScore) => prevSum + turnScore, 0 ))
-}
+    if (outcome === "Y") {
+      // need to draw
+      return 3 + opponentMove; // draw score + ourChoice score (same as opponent move when draw)
+    }
 
-part1(data)
-part2(data)
+    // else we need to win
+    const winningMove = opponentMove + 1;
+    return (
+      (winningMove > 3 // out of bounds
+        ? 1
+        : winningMove) + 6
+    ); // add winner score
+  });
+
+  console.log(turns.reduce((prevSum, turnScore) => prevSum + turnScore, 0));
+};
+
+part1(data);
+part2(data);
