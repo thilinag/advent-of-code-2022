bfcc47118d8baa640c76da11ba245061a89c6b3d
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-04 19:16:20 +1030

    run prettier on template

--- template.mjs
+++ template.mjs
@@ -1,18 +1,14 @@
-import { readFileSync } from 'fs'
-const input = readFileSync('./input.txt', { encoding: 'utf8' })
-const data = input.split('\n')
+import { readFileSync } from "fs";
+const input = readFileSync("./input.txt", { encoding: "utf8" });
+const data = input.split("\n");
 
 const part1 = (data) => {
-
-
-  console.log()
-}
+  console.log();
+};
 
 const part2 = (data) => {
+  console.log();
+};
 
-
-  console.log()
-}
-
-part1(data)
-part2(data)
+part1(data);
+part2(data);
