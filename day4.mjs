f0a8fc82c5e5f5981b2c9be91722c7839456f61c
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-04 15:53:17 +1030

    day 4 solution

---
+++ day4.mjs
@@ -0,0 +1,27 @@
+import { readFileSync } from "fs";
+const input = readFileSync("./input.txt", { encoding: "utf8" });
+const data = input.split("\n");
+
+const part1 = (data) => {
+  const overlappingPairs = data.filter((task) => {
+    const tasks = task.split(",").map((r) => r.split("-").map(Number));
+    return (
+      (tasks[0][0] <= tasks[1][0] && tasks[0][1] >= tasks[1][1]) ||
+      (tasks[1][0] <= tasks[0][0] && tasks[1][1] >= tasks[0][1])
+    );
+  });
+
+  console.log(overlappingPairs.length);
+};
+
+const part2 = (data) => {
+  const overlappingPairs = data.filter((task) => {
+    const tasks = task.split(",").map((r) => r.split("-").map(Number));
+    return !(tasks[0][1] < tasks[1][0] || tasks[1][1] < tasks[0][0]);
+  });
+
+  console.log(overlappingPairs.length);
+};
+
+part1(data);
+part2(data);
