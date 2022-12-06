87f988417c4e6b9d4bee88e2e91c97d848b63406
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-06 16:02:33 +1030

    prettier

--- day6.mjs
+++ day6.mjs
@@ -1,33 +1,33 @@
 import { readFileSync } from "fs";
 const input = readFileSync("./input.txt", { encoding: "utf8" });
-const data = input.split('');
+const data = input.split("");
 
 const getMarker = (sequence) => {
   let paketMarker;
   for (let i = sequence - 1; i < data.length; i++) {
-    const set = new Set()
+    const set = new Set();
     for (let j = 0; j < sequence; j++) {
-      set.add(data[i-j])
+      set.add(data[i - j]);
     }
     if (set.size === sequence) {
-      paketMarker = i + 1
-      break
+      paketMarker = i + 1;
+      break;
     }
   }
 
-  return paketMarker
-}
+  return paketMarker;
+};
 
 const part1 = (data) => {
   const sequence = 4;
 
-  console.log(getMarker(sequence))
+  console.log(getMarker(sequence));
 };
 
 const part2 = (data) => {
   const sequence = 14;
-  
-  console.log(getMarker(sequence))
+
+  console.log(getMarker(sequence));
 };
 
 part1(data);
