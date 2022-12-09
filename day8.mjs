07e5914edccaf24c4622900f6ba3555a24c18108
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-09 13:46:10 +1030

    move covered logic to fn

--- day8.mjs
+++ day8.mjs
@@ -2,28 +2,31 @@ import { readFileSync } from "fs";
 const input = readFileSync("./input.txt", { encoding: "utf8" });
 const data = input.split("\n").map((row) => row.split("").map(Number));
 
+const isCovered = (treeRow, currentTreeHeight) =>
+  treeRow.some((treeHeight) => treeHeight >= currentTreeHeight);
+
 const part1 = (data) => {
   let coveredTrees = 0;
 
   // ignore outer edge so we start from index 1 <---> length - 1 horizontal and vertical
   for (let i = 1; i < data.length - 1; i++) {
     for (let j = 1; j < data[0].length - 1; j++) {
-      const tree = data[i][j];
+      const currentTreeHeight = data[i][j];
 
       const topTrees = data.slice(0, i).map((row) => row[j]);
-      const topCovered = topTrees.some((h) => h >= tree);
+      const topCovered = isCovered(topTrees, currentTreeHeight);
       if (!topCovered) continue;
 
       const rightTrees = data[i].slice(j + 1);
-      const rightCovered = rightTrees.some((h) => h >= tree);
+      const rightCovered = isCovered(rightTrees, currentTreeHeight);
       if (!rightCovered) continue;
 
       const bottomTrees = data.slice(i + 1).map((row) => row[j]);
-      const bottomCovered = bottomTrees.some((h) => h >= tree);
+      const bottomCovered = isCovered(bottomTrees, currentTreeHeight);
       if (!bottomCovered) continue;
 
       const leftTrees = data[i].slice(0, j);
-      const leftCovered = leftTrees.some((h) => h >= tree);
+      const leftCovered = isCovered(leftTrees, currentTreeHeight);
       if (!leftCovered) continue;
 
       // if we are here that means we are coverd in all directions
@@ -42,34 +45,25 @@ const part2 = (data) => {
   // ignore outer edge so we start from index 1 <---> length - 1 horizontal and vertical
   for (let i = 1; i < data.length - 1; i++) {
     for (let j = 1; j < data[0].length - 1; j++) {
-      const tree = data[i][j];
-      const view = [];
+      const currentTreeHeight = data[i][j];
 
       const topTrees = data.slice(0, i).map((row) => row[j]);
-      if (topTrees.length) {
-        // reverse before pushing since we are looking towards top
-        view.push(topTrees.reverse());
-      }
-
       const rightTrees = data[i].slice(j + 1);
-      if (rightTrees.length) {
-        view.push(rightTrees);
-      }
-
       const bottomTrees = data.slice(i + 1).map((row) => row[j]);
-      if (bottomTrees.length) {
-        view.push(bottomTrees);
-      }
-
       const leftTrees = data[i].slice(0, j);
-      if (leftTrees.length) {
-        // reverse before pushing since we are looking towards left
-        view.push(leftTrees.reverse());
-      }
+
+      const view = [
+        topTrees.reverse(), // reversing since we are looking towards top
+        rightTrees,
+        bottomTrees,
+        leftTrees.reverse(), // reversing since we are looking towards left
+      ];
 
       // scenic score is found by multiplying together its viewing distance
       const scenicScore = view.reduce((accumulator, trees) => {
-        const blockingTreeIndex = trees.findIndex((t) => t >= tree);
+        const blockingTreeIndex = trees.findIndex(
+          (treeHeight) => treeHeight >= currentTreeHeight
+        );
         // if we can't find any blocking trees that means we can see all the avialable trees to that direction
         // findIndex gives -1 in this sittuation otherwise it gives us the index of blocking tree
         const viewingDistance =
