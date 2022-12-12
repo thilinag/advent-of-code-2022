8b9beb8fa530f571a9b650ec3d973e234b64bef0
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-12 16:44:02 +1030

    rename variables

--- day9.mjs
+++ day9.mjs
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 const input = readFileSync("./input.txt", { encoding: "utf8" });
 const data = input.split("\n");
 
-const getTailPositions = (knots) => {
+const getKnotPositions = (knots) => {
   // create placeholder for knot positions
   const knotPositions = Array.from(Array(knots), () => [[0, 0]]);
   let knot = 0;
@@ -12,78 +12,56 @@ const getTailPositions = (knots) => {
       const [x, y] = knotPositions[knot].at(-1);
 
       // move head
+      let tailPosition;
       switch (direction) {
         case "R":
-          knotPositions[knot].push([x + 1, y]);
+          tailPosition = [x + 1, y];
           break;
         case "D":
-          knotPositions[knot].push([x, y - 1]);
+          tailPosition = [x, y - 1];
           break;
         case "L":
-          knotPositions[knot].push([x - 1, y]);
+          tailPosition = [x - 1, y];
           break;
         case "U":
-          knotPositions[knot].push([x, y + 1]);
+          tailPosition = [x, y + 1];
           break;
       }
+      knotPositions[0].push(tailPosition);
 
       // move tails (head is knot 0)
       for (let k = 1; k < knots; k++) {
-        const [x, y] = knotPositions[k - 1].at(-1);
-        const [tx, ty] = knotPositions[k].at(-1);
-        const d = Math.max(Math.abs(tx - x), Math.abs(ty - y));
+        const [prevX, prevY] = knotPositions[k - 1].at(-1);
+        let [currentX, currentY] = knotPositions[k].at(-1);
+        const d = Math.max(
+          Math.abs(currentX - prevX),
+          Math.abs(currentY - prevY)
+        );
+        let newTailPosition;
         if (d > 1) {
-          if (ty === y) {
+          if (currentY === prevY) {
             // go horizontal
-            knotPositions[k].push([
-              (knotPositions[k].at(-1)[0] += x > tx ? 1 : -1),
-              ty,
-            ]);
-          } else if (tx == x) {
+            newTailPosition = [
+              (currentX += prevX > currentX ? 1 : -1),
+              currentY,
+            ];
+          } else if (currentX == prevX) {
             // go vertical
-            knotPositions[k].push([
-              tx,
-              (knotPositions[k].at(-1)[1] += y > ty ? 1 : -1),
-            ]);
+            newTailPosition = [
+              currentX,
+              (currentY += prevY > currentY ? 1 : -1),
+            ];
           } else {
             // go diagonal
-            knotPositions[k].push([
-              (knotPositions[k].at(-1)[0] += x > tx ? 1 : -1),
-              (knotPositions[k].at(-1)[1] += y > ty ? 1 : -1),
-            ]);
+            newTailPosition = [
+              (currentX += prevX > currentX ? 1 : -1),
+              (currentY += prevY > currentY ? 1 : -1),
+            ];
           }
+
+          knotPositions[k].push(newTailPosition);
         }
       }
-
-      // visualize
-      // const c = knotPositions.map((t) => `${t.at(-1)[0]}-${t.at(-1)[1]}`);
-      // const max = Math.max(
-      //   ...knotPositions.map((t) => Math.max(...t.map((u) => Math.max(...u))))
-      // );
-
-      // for (let i = 0; i <= max; i++) {
-      //   let a = "";
-      //   for (let j = 0; j <= max; j++) {
-      //     const b = c.indexOf(`${i}-${j}`);
-      //     const lastIndex = c.length - 1;
-      //     if (b > -1) {
-      //       switch (b) {
-      //         case 0:
-      //           a += "H";
-      //           break;
-      //         case lastIndex:
-      //           a += "T";
-      //           break;
-      //         default:
-      //           a += b;
-      //       }
-      //     } else {
-      //       a += ".";
-      //     }
-      //   }
-      //   console.log(a);
-      // }
-      // console.log(`-------------------`);
     }
   }
 
@@ -91,13 +69,11 @@ const getTailPositions = (knots) => {
 };
 
 const getUniqueTailPositions = (knots) => {
-  const tailPositions = getTailPositions(knots);
+  const knotPositions = getKnotPositions(knots);
+  const tailPositions = knotPositions.at(-1);
 
   // How many positions does the tail of the rope visit at least once?
-  // adding 1 for the starting position
-  return (
-    new Set([...tailPositions.at(-1).map(([x, y]) => `${x}-${y}`)]).size + 1
-  );
+  return new Set([...tailPositions.map(([x, y]) => `${x}-${y}`)]).size;
 };
 
 const part1 = (knots) => {
