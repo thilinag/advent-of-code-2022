b931b7f7698b12fa4ecae9401493453b6b8613b5
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-26 23:35:51 +1030

    day 20 part 2

--- day20.mjs
+++ day20.mjs
@@ -7,28 +7,30 @@ const input = readFileSync(`./${inputFileName}.txt`, { encoding: "utf8" });
 // const numbers = {};
 const data = input.split("\n").map(Number);
 
-const mix = (numbers, data) => {
-  for (let i = 0; i < data.length; i++) {
-    const currentId = numbers.findIndex((num) => num.pos === i);
-    numbers.splice(currentId, 1);
-    numbers.splice((data[i] + currentId) % numbers.length, 0, {
-      num: data[i],
-      pos: i,
-    });
+const mix = (numbers, data, times) => {
+  for (let time = 0; time < times; time++) {
+    for (let i = 0; i < data.length; i++) {
+      const currentId = numbers.findIndex((num) => num.pos === i);
+      numbers.splice(currentId, 1);
+      numbers.splice((data[i] + currentId) % numbers.length, 0, {
+        num: data[i],
+        pos: i,
+      });
+    }
   }
 
   return numbers;
 };
 
-const part1 = (data, positions) => {
+const part1 = (data) => {
   let special = [];
   let numbers = data.map((num, index) => ({ num, pos: index }));
 
-  const mixedNumbers = mix(numbers, data);
+  const mixedNumbers = mix(numbers, data, 1);
 
   const zeroIndex = mixedNumbers.findIndex((number) => number.num === 0);
 
-  for (const position of positions) {
+  for (const position of [1000, 2000, 3000]) {
     special.push(
       mixedNumbers[(position + zeroIndex) % mixedNumbers.length].num
     );
@@ -37,13 +39,33 @@ const part1 = (data, positions) => {
   console.log(special.reduce((p, c) => p + c, 0));
 };
 
-const part2 = (data) => {
-  console.log();
+const part2 = (data, decryptionKey) => {
+  let special = [];
+  let numbers = data.map((num, index) => ({
+    number: num * decryptionKey,
+    pos: index,
+  }));
+
+  const mixedNumbers = mix(
+    numbers,
+    data.map((num) => num * decryptionKey),
+    10
+  );
+
+  const zeroIndex = mixedNumbers.findIndex((number) => number.num === 0);
+
+  for (const position of [1000, 2000, 3000]) {
+    special.push(
+      mixedNumbers[(position + zeroIndex) % mixedNumbers.length].num
+    );
+  }
+
+  console.log(special.reduce((p, c) => p + c, 0));
 };
 
 console.time("part1");
-part1(data, [1000, 2000, 3000]);
+part1(data);
 console.timeEnd("part1");
 console.time("part2");
-part2(data);
+part2(data, 811589153);
 console.timeEnd("part2");
