0cd8e5dbfb88658de8b85d1bf5d87659e5dac600
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2022-12-31 16:51:31 +1030

    day 18 part 2

--- day18.mjs
+++ day18.mjs
@@ -33,11 +33,98 @@ const part1 = (data) => {
   }
 
   // What is the surface area of your scanned lava droplet?
-  console.log({ surfaces });
+  console.log(surfaces);
+};
+
+// https://en.wikipedia.org/wiki/Breadth-first_search
+// 1  procedure BFS(G, root) is
+const fillOutside = (cubes, [minX, minY, minZ], [maxX, maxY, maxZ]) => {
+  let surfaces = 0;
+  // 2 let Q be a queue
+  const queue = [];
+  const visited = new Set();
+  // 3 label root as explored
+  visited.add(`${minX},${minY},${minZ}`);
+  // 4 Q.enqueue(root)
+  queue.push({ x: minX, y: minY, z: minZ });
+  // 5 while Q is not empty do
+  while (queue.length > 0) {
+    // 6 v := Q.dequeue()
+    const { x, y, z } = queue.shift();
+    // 7 if v is the goal then
+    // 8 return v
+    // 9 for all edges from v to w in G.adjacentEdges(v) do
+    const neighbors = [
+      { x: x + 1, y, z },
+      { x: x - 1, y, z },
+      { x, y: y + 1, z },
+      { x, y: y - 1, z },
+      { x, y, z: z + 1 },
+      { x, y, z: z - 1 },
+    ];
+
+    for (const neighbor of neighbors) {
+      const { x, y, z } = neighbor;
+      const key = `${x},${y},${z}`;
+
+      if (cubes.has(key)) {
+        // this is touching the lava droplet
+        surfaces++;
+        continue;
+      }
+
+      // 10 if w is not labeled as explored then
+      if (visited.has(key)) {
+        continue;
+      }
+
+      // 11 label w as explored
+      visited.add(key);
+
+      // 12 w.parent := v
+      if (
+        x > maxX ||
+        y > maxY ||
+        z > maxZ ||
+        x < minX ||
+        y < minY ||
+        z < minZ
+      ) {
+        // point is out of bounds
+        continue;
+      }
+
+      // 13 Q.enqueue(w)
+      queue.push(neighbor);
+    }
+  }
+
+  return surfaces;
 };
 
 const part2 = (data) => {
-  console.log();
+  const cubes = new Set(data);
+  const cubePositions = data.map((line) => line.split(",").map(Number));
+  const xPos = [];
+  const yPos = [];
+  const zPos = [];
+  cubePositions.forEach((pos) => {
+    xPos.push(pos[0]);
+    yPos.push(pos[1]);
+    zPos.push(pos[2]);
+  });
+
+  // make a cube 1 layer larger than the lava droplet,
+  // then use DFS to fill it and find outside faces
+  const minX = Math.min(...xPos) - 1;
+  const maxX = Math.max(...xPos) + 1;
+  const minY = Math.min(...yPos) - 1;
+  const maxY = Math.max(...yPos) + 1;
+  const minZ = Math.min(...zPos) - 1;
+  const maxZ = Math.max(...zPos) + 1;
+
+  // What is the exterior surface area of your scanned lava droplet?
+  console.log(fillOutside(cubes, [minX, minY, minZ], [maxX, maxY, maxZ]));
 };
 
 console.time("part1");
